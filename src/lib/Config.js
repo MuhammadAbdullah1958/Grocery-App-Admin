577fa6bd35dfb48808cd4b24d77f2d8c743b8b98
iamabdullah <abdullah.infinitybits@gmail.com>
2023-04-07 16:47:03 +0500

    domain updated

--- src/lib/Config.js
+++ src/lib/Config.js
@@ -1,4 +1,5 @@
-const Domain = "http://localhost:3002";
+// const Domain = "http://loca192.168.18.632";
+const Domain = "http://192.168.18.63:3002";
 export const AdminDomain = "http://localhost:3001"
 //const Domain = "http://192.168.100.134:8000";
 //const Domain = "http://159.203.180.32:8000";
